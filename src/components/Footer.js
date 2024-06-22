8a4079039b96632eaa8a2740a78f74c7f3523727
TinhNguyenTB <tinh271103@gmail.com>
2024-06-22 16:43:40 +0700

    design footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer className='bg-slate-200'>
+            <div className='container mx-auto p-6'>
+                <div className='mx-auto w-fit flex'>Copyright 2024 © <p className='font-bold'> E-commerce</p></div>
+            </div>
+
+        </footer>
     )
 }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,22 +5,42 @@ import { Link } from 'react-router-dom';
 
 const Login = () => {
     const [showPassword, setShowPassword] = useState(false);
+    const [data, setData] = useState({
+        email: "",
+        password: ""
+    });
+    const handleOnchange = (e) => {
+        const { name, value } = e.target;
+        setData((prev) => {
+            return {
+                ...prev,
+                [name]: value
+            }
+        });
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
 
     return (
         <section id='login'>
             <div className='mx-auto container p-6'>
-                <div className='bg-white w-full px-2 py-5 max-w-md mx-auto rounded'>
+                <div className='bg-white w-full px-4 py-5 max-w-sm mx-auto rounded'>
 
                     <div className='w-fit mx-auto text-7xl'>
                         <FaUserAstronaut />
                     </div>
 
-                    <form>
+                    <form className='pt-6' onSubmit={handleSubmit}>
                         <div className='grid'>
                             <label>Email:</label>
                             <div className='bg-slate-100 p-2'>
                                 <input type='email' placeholder='Enter email'
                                     className='w-full h-full outline-none bg-transparent rounded'
+                                    name='email'
+                                    value={data.email}
+                                    onChange={(event) => handleOnchange(event)}
                                 />
                             </div>
                         </div>
@@ -30,6 +50,9 @@ const Login = () => {
                                 <input type={showPassword ? 'text' : 'password'}
                                     placeholder='Enter password'
                                     className='w-full h-full outline-none bg-transparent rounded'
+                                    name='password'
+                                    value={data.password}
+                                    onChange={(event) => handleOnchange(event)}
                                 />
                                 <div className='cursor-pointer text-xl'
                                     onClick={() => setShowPassword((prev) => !prev)}
@@ -43,10 +66,13 @@ const Login = () => {
                                 Forgot password ?
                             </Link>
                         </div>
-                        <button className='bg-red-500 text-white px-6 py-2 w-full max-w-[150px] rounded-full hover:scale-110 transition-all mx-auto block mt-6'>
+                        <button className='bg-red-500 hover:bg-red-600 text-white px-6 py-2 w-full max-w-[150px] rounded-full hover:scale-110 transition-all mx-auto block mt-6'>
                             Login
                         </button>
                     </form>
+                    <p className='my-4 mx-auto w-fit'>
+                        Don't have an account ? <Link to={"/sign-up"} className='text-red-500 hover:underline hover:text-red-600 transition-all'>Sign up</Link>
+                    </p>
                 </div>
             </div>
         </section>
